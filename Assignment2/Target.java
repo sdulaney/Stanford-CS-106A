fed964bb13bbd6d4520cac19d71efecd779565e8
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-01-06 15:15:35 -0800

    add solution for target problem

--- Assignment2/Target.java
+++ Assignment2/Target.java
@@ -13,5 +13,18 @@ import java.awt.*;
 public class Target extends GraphicsProgram {	
 	public void run() {
 		/* You fill this in. */
+		drawCircle(72, Color.RED);
+		drawCircle(0.65 * 72, Color.WHITE);
+		drawCircle(0.3 * 72, Color.RED);
+	}
+	
+	public void drawCircle(double radius, Color c) {
+		double x = getWidth() / 2 - radius / 2;
+		double y = getHeight() / 2 - radius / 2;
+		GOval circle = new GOval(radius, radius);
+		circle.setFilled(true);
+		circle.setFillColor(c);
+		circle.move(x, y);
+		add(circle);
 	}
 }
