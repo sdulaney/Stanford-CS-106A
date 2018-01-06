a19abe6c82cf7e0d9d18db2a3b288c2fe7faef73
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-01-06 14:46:27 -0800

    add solution for pyramid problem

--- Assignment2/Pyramid.java
+++ Assignment2/Pyramid.java
@@ -27,6 +27,19 @@ public class Pyramid extends GraphicsProgram {
 	
 	public void run() {
 		/* You fill this in. */
+		putAllBricks();
+	}
+	
+	private void putAllBricks() {
+		for(int i = 0; i < BRICKS_IN_BASE; i++) {
+			int bricksInRow = BRICKS_IN_BASE - i;
+			for(int j = 0; j < bricksInRow; j++) {
+				double x = getWidth() / 2 - (bricksInRow * BRICK_WIDTH) / 2 + (j * BRICK_WIDTH);
+				double y = getHeight() - BRICK_HEIGHT * (i + 1);
+				GRect brick = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
+				add(brick);
+			}
+		}
 	}
 }
 
