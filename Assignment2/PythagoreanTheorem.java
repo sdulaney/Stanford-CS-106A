4adb403dd7e577443752f471aac7304291591397
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-01-07 12:15:30 -0800

    add solution for pythagorean theorem problem

--- Assignment2/PythagoreanTheorem.java
+++ Assignment2/PythagoreanTheorem.java
@@ -11,5 +11,18 @@ import acm.program.*;
 public class PythagoreanTheorem extends ConsoleProgram {
 	public void run() {
 		/* You fill this in */
+		sayWelcome();
+		calculateHypotenuse();	
+	}
+	
+	public void sayWelcome() {
+		println("Enter values to compute Pythagorean theorem.");
+	}
+	
+	public void calculateHypotenuse() {
+		int a = readInt("a: ");
+		int b = readInt("b: ");
+		double c = Math.sqrt((a * a) + (b * b));
+		println("c: " + c);
 	}
 }
