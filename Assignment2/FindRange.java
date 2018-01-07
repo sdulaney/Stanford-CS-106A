aef0c2139d91d52a011fd3626fe321c6b9e6a2e2
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-01-07 12:31:47 -0800

    add solution for find range problem

--- Assignment2/FindRange.java
+++ Assignment2/FindRange.java
@@ -9,8 +9,35 @@
 import acm.program.*;
 
 public class FindRange extends ConsoleProgram {
+	private static final int sentinel = 0;
 	public void run() {
 		/* You fill this in */
+		sayWelcome();
+		findRange();
+	}
+	
+	public void sayWelcome() {
+		println("This program finds the largest and smallest numbers.");
+	}
+	
+	public void findRange() {
+		int inputNum = readInt("? ");
+		if(inputNum == sentinel) {
+			println("Please enter at least one number.");
+		}
+		int min = inputNum;
+		int max = inputNum;
+		while(inputNum != sentinel) {
+			inputNum = readInt("? ");
+			if(inputNum < min) {
+				min = inputNum;
+			}
+			if(inputNum > max) {
+				max = inputNum;
+			}
+		}
+		println("smallest: " + min);
+		println("largest: " + max);
 	}
 }
 
