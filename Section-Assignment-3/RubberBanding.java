7c6e463cb6c22586365a5ce4c0ba50361655eb53
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-01-08 18:24:14 -0800

    add solution for rubber banding problem

--- Section-Assignment-3/RubberBanding.java
+++ Section-Assignment-3/RubberBanding.java
@@ -12,6 +12,27 @@ import acm.graphics.*;
 import acm.program.*;
 import java.awt.event.*;
 
+/** This class allows users to drag lines on the canvas */
 public class RubberBanding extends GraphicsProgram {
-
+	public void run() {
+		addMouseListeners();
+	}
+	
+	/** Called on mouse press to create a new line */
+	public void mousePressed(MouseEvent e) {
+		double x = e.getX();
+		double y = e.getY();
+		line = new GLine(x, y, x, y);
+		add(line);
+	}
+	
+	/** Called on mouse drag to reset the endpoint */
+	public void mouseDragged(MouseEvent e) {
+		double x = e.getX();
+		double y = e.getY();
+		line.setEndPoint(x,  y);
+	}
+	
+	/* Private instance variables */
+	private GLine line;
 }
