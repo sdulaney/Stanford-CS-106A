46b492e4a6f1bd0ed358c9d4b987ac73fbab4a5a
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-01-08 17:50:39 -0800

    add section assignment 3 files

---
+++ Section-Assignment-3/RubberBanding.java
@@ -0,0 +1,17 @@
+/*
+ * File: RubberBanding.java
+ * Name: Stewart Dulaney
+ * --------------------
+ * This program allows users to create lines on the graphics
+ * canvas by clicking and dragging with the mouse. The line
+ * is redrawn from the original point to the new endpoint, which
+ * makes it look as if it is connected with a rubber band.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.event.*;
+
+public class RubberBanding extends GraphicsProgram {
+
+}
