d03e11b55d08a4637d154c1fd64eb1c5cda35371
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-01-08 16:26:36 -0800

    add solution for fibonacci problem

--- Section-Assignment-2/Fibonacci.java
+++ Section-Assignment-2/Fibonacci.java
@@ -1,4 +1,25 @@
+/*
+ * File: Fibonacci.java
+ * Name: 
+ * Section Leader: 
+ * --------------------
+ * This file is the starter file for the Fibonacci problem.
+ */
 
-public class Fibonacci {
+import acm.program.*;
 
+public class Fibonacci extends ConsoleProgram {
+	public void run() {
+		println("This program lists the Fibonacci sequence.");
+		int t1 = 0;
+		int t2 = 1;
+		while(t1 < MAX_TERM_VALUE) {
+			println(t1);
+			int t3 = t1 + t2;
+			t1 = t2;
+			t2 = t3;
+		}
+	}
+	
+	private static final int MAX_TERM_VALUE = 10000;
 }
