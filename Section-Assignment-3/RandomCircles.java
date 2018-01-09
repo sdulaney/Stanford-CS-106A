70a02d9122363699e7cd48cf9c6e5796f5b8b874
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-01-08 18:07:24 -0800

    add solution for random circles problem

--- Section-Assignment-3/RandomCircles.java
+++ Section-Assignment-3/RandomCircles.java
@@ -15,5 +15,26 @@ import acm.graphics.*;
 import acm.util.*;
 
 public class RandomCircles extends GraphicsProgram {
-
+	
+	/** Number of circles */
+	private static final int NCIRCLES = 10;
+	/** Minimum radius */
+	private static final double MIN_RADIUS = 5;
+	/** Maximum radius */
+	private static final double MAX_RADIUS = 50;
+	
+	public void run() {
+		for(int i = 0; i < NCIRCLES; i++) {
+			double radius = rgen.nextDouble(MIN_RADIUS, MAX_RADIUS);
+			double x = rgen.nextDouble(0, getWidth() - radius * 2);
+			double y = rgen.nextDouble(0, getHeight() - radius * 2);
+			GOval circle = new GOval(x, y, radius * 2, radius * 2);
+			circle.setFilled(true);
+			circle.setColor(rgen.nextColor());
+			add(circle);
+		}
+	}
+	
+	/* Private instance variable */
+	private RandomGenerator rgen = RandomGenerator.getInstance();
 }
