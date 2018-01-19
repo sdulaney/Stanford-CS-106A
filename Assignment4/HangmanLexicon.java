39c3e5e4417a2be534ab2ca3d986bb9ec2871d56
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-01-19 14:31:01 -0800

    read the lexicon from a data file

--- Assignment4/HangmanLexicon.java
+++ Assignment4/HangmanLexicon.java
@@ -6,30 +6,39 @@
  */
 
 import acm.util.*;
+import java.util.*;
+import java.io.*;
 
 public class HangmanLexicon {
+			
+	// This is the HangmanLexicon constructor
+	public HangmanLexicon() {
+		wordList = new ArrayList<String>();	
+		try {
+			BufferedReader rd = new BufferedReader(new FileReader("HangmanLexicon.txt"));
+			while (true) {
+				String line = rd.readLine();
+				wordList.add(line);
+				if (line == null) {
+					break;
+				}
+			}
+		} catch (IOException ex) {
+			throw new ErrorException(ex);
+		}
+	}
 	
 	/** Returns the number of words in the lexicon. */
 	public int getWordCount() {
-		return 10;
+		return wordList.size();
 	}
 	
 	/** Returns the word at the specified index. */
 	public String getWord(int index) {
-		switch (index) {
-			case 0: return "BUOY";
-			case 1: return "COMPUTER";
-			case 2: return "CONNOISSEUR";
-			case 3: return "DEHYDRATE";
-			case 4: return "FUZZY";
-			case 5: return "HUBBUB";
-			case 6: return "KEYHOLE";
-			case 7: return "QUAGMIRE";
-			case 8: return "SLITHER";
-			case 9: return "ZIRCON";
-			default: throw new ErrorException("getWord: Illegal index");
-		}
+		return wordList.get(index);
 	}
 	
+	/* Private instance variables */
+	private ArrayList<String> wordList;
 }
 
