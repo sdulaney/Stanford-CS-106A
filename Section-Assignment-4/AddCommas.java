8e4c7d1197c314386469cc7330f0ed453773fe2c
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-01-22 13:33:19 -0800

    add solution for add commas problem

--- Section-Assignment-4/AddCommas.java
+++ Section-Assignment-4/AddCommas.java
@@ -1,4 +1,36 @@
+/*
+ * File: AddCommas.java
+ * -------------------
+ * This program display large numbers
+ * by using commas to separate digits into 
+ * groups of three.
+ */
 
-public class AddCommas {
+import acm.program.*;
 
+public class AddCommas extends ConsoleProgram {
+	
+	public void run() {
+		while (true) {
+			String digits = readLine("Enter a numeric string: ");
+			if (digits.length() == 0) {
+				break;
+			}
+			println(addCommasToNumericString(digits));
+		}
+	}
+	
+	private String addCommasToNumericString(String digits) {
+		String result = "";
+		int len = digits.length();
+		int numDigits = 0;
+		for (int i = len - 1; i >= 0; i--) {
+			result = digits.charAt(i) + result;
+			numDigits++;
+			if (((numDigits % 3) == 0) && (i > 0)) {
+				result = "," + result;
+			}
+		}
+		return result;
+	}
 }
