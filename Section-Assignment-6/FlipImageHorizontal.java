ff4793fc5a4c1dca3d5e298a1cbb178c43845387
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-01-22 15:52:24 -0800

    add solution for flip image horizontal problem

--- Section-Assignment-6/FlipImageHorizontal.java
+++ Section-Assignment-6/FlipImageHorizontal.java
@@ -1,4 +1,34 @@
+/*
+ * File: FlipImageHorizontal.java
+ * This program reverses a picture in the 
+ * horizontal dimension.
+ */
 
-public class FlipImageHorizontal {
+import acm.program.*;
+import acm.graphics.*;
 
+public class FlipImageHorizontal extends GraphicsProgram {
+
+	public void run() {
+		GImage image = new GImage("milkmaid.jpeg");
+		add(image);
+		GImage flippedImage = flipHorizontal(image);
+		add(flippedImage);
+		flippedImage.move(250, 0);
+	}
+	
+	private GImage flipHorizontal(GImage image) {
+		int[][] array = image.getPixelArray();
+		int width = array[0].length;
+		int height = array.length;
+		for (int row = 0; row < height; row++) {
+			for (int p1 = 0; p1 < width / 2; p1++) {
+				int p2 = width - p1 - 1;
+				int temp = array[row][p1];
+				array[row][p1] = array[row][p2];
+				array[row][p2] = temp;
+			}
+		}
+		return new GImage(array);
+	}
 }
