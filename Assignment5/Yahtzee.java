56c307e1fa66b18bd6323e4435f3969397739dfc
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-01-25 15:10:08 -0800

    implement playGame and roll methods

--- Assignment5/Yahtzee.java
+++ Assignment5/Yahtzee.java
@@ -27,9 +27,55 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 
 	private void playGame() {
 		/* You fill this in */
+		scorecard = new int[N_CATEGORIES][nPlayers];
+		for (int turns = 0; turns < N_SCORING_CATEGORIES; turns++) {
+			for(int player = 1; player <=  nPlayers; player++) {
+				startFirstDiceRoll(player);
+				secondAndThirdRoll();
+				selectCategory(player);
+			}
+		}
+		totalUpResults();
+		calculateWinner();
+	}
+	
+	private void startFirstDiceRoll(int player) {
+		display.printMessage(playerNames[player - 1] + "'s turn! Click \"Roll Dice\" button to roll the dice.");
+		display.waitForPlayerToClickRoll(player);
+		for (int dice = 0; dice < N_DICE; dice++) {
+			diceRoll[dice] = rgen.nextInt(1, 6);
+		}
+		display.displayDice(diceRoll);
+	}
+	
+	private void secondAndThirdRoll() {
+		for (int i = 0; i < 2; i++) {
+			display.printMessage("Select the dice you wish to re-roll and click \"Roll Again\"");
+			display.waitForPlayerToSelectDice();
+			for(int dice = 0; dice < N_DICE; dice++) {
+				if (display.isDieSelected(dice)) {
+					diceRoll[dice] = rgen.nextInt(1, 6);
+				}
+			}
+			display.displayDice(diceRoll);
+		}		
+	}
+	
+	private void selectCategory(int player) {
+		
+	}
+	
+	private void totalUpResults() {
+		
+	}
+	
+	private void calculateWinner() {
+		
 	}
 		
 /* Private instance variables */
+	private int[][] scorecard;
+	private int[] diceRoll = new int[N_DICE];
 	private int nPlayers;
 	private String[] playerNames;
 	private YahtzeeDisplay display;
