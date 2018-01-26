aaf3e318082299de959f3e6135f2b99bf845832f
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-01-26 15:16:24 -0800

    add section assignment 7 files

---
+++ Section-Assignment-7/BoxDiagram.java
@@ -0,0 +1,9 @@
+/*
+ * File: BoxDiagram.java
+ * This program allows the user to create a set of boxes with labels
+ * and then drag them around in the window.
+ */
+
+public class BoxDiagram {
+
+}
