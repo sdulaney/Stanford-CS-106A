7fc69be3ac424ea346ed5fabd6eb81270e70b1f0
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-02-06 17:49:15 -0800

    assemble the gui interactors

--- Assignment7/FacePamphlet.java
+++ Assignment7/FacePamphlet.java
@@ -11,7 +11,7 @@ import acm.util.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class FacePamphlet extends Program 
+public class FacePamphlet extends ConsoleProgram 
 					implements FacePamphletConstants {
 
 	/**
@@ -20,7 +20,48 @@ public class FacePamphlet extends Program
 	 * initialization that needs to be performed.
 	 */
 	public void init() {
-		// You fill this in
+		
+		nameLabel = new JLabel("Name");
+		add(nameLabel, NORTH);
+		
+		nameField = new JTextField(TEXT_FIELD_SIZE);
+		add(nameField, NORTH);
+		
+		addButton = new JButton("Add");
+		add(addButton, NORTH);
+		
+		deleteButton = new JButton("Delete");
+		add(deleteButton, NORTH);
+		
+		lookupButton = new JButton("Lookup");
+		add(lookupButton, NORTH);
+		
+		changeStatusField = new JTextField(TEXT_FIELD_SIZE);
+		add(changeStatusField, WEST);
+		changeStatusField.addActionListener(this);
+		
+		changeStatusButton = new JButton("Change Status");
+		add(changeStatusButton, WEST);
+		
+		add(new JLabel(EMPTY_LABEL_TEXT), WEST);
+		
+		changePictureField = new JTextField(TEXT_FIELD_SIZE);
+		add(changePictureField, WEST);
+		changePictureField.addActionListener(this);
+		
+		changePictureButton = new JButton("Change Picture");
+		add(changePictureButton, WEST);
+		
+		add(new JLabel(EMPTY_LABEL_TEXT), WEST);
+		
+		addFriendField = new JTextField(TEXT_FIELD_SIZE);
+		add(addFriendField, WEST);
+		addFriendField.addActionListener(this);
+		
+		addFriendButton = new JButton("Add Friend");
+		add(addFriendButton, WEST);
+		
+		addActionListeners();
     }
     
   
@@ -30,7 +71,33 @@ public class FacePamphlet extends Program
      * to respond to these actions.
      */
     public void actionPerformed(ActionEvent e) {
-		// You fill this in as well as add any additional methods
+		Object source = e.getSource();
+		if (source == addButton) {
+			println("Add: " + nameField.getText());
+		} else if (source == deleteButton) {
+			println("Delete: " + nameField.getText());
+		} else if (source == lookupButton) {
+			println("Lookup: " + nameField.getText());
+		} else if (source == changeStatusButton || source == changeStatusField) {
+			println("Change Status: " + changeStatusField.getText());
+		} else if (source == changePictureButton || source == changePictureField) {
+			println("Change Picture: " + changePictureField.getText());
+		} else if (source == addFriendButton || source == addFriendField) {
+			println("Add Friend: " + addFriendField.getText());
+		}
 	}
+
+    /* Private instance variables */
+    private JLabel nameLabel;
+    private JTextField nameField;
+    private JButton addButton;
+    private JButton deleteButton;
+    private JButton lookupButton;
+    private JTextField changeStatusField;
+    private JButton changeStatusButton;
+    private JTextField changePictureField;
+    private JButton changePictureButton;
+    private JTextField addFriendField;
+    private JButton addFriendButton;
 
 }
